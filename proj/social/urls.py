b6c2d9ba3cd7ad71482949c28e18311af8653a3d
GD <63779654+gd2003gamingchamp@users.noreply.github.com>
2022-09-25 16:03:42 +0530

    Basic UI added issue #5

--- proj/social/urls.py
+++ proj/social/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('components-alerts', views.comp_alert, name='comp_alert'),
+    path('users-profile',views.usr_profile, name='usr_profile'),
 ]
